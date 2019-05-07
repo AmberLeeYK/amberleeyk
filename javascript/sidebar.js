35cf635bebc8ba15c8ada2bc0b8ea6ce2691c3ad
Sarah Lee <sarahjayeunlee@gmail.com>
2019-05-07 15:30:17 -0400

    fixed sidebar margins

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginRight= "0"; /* original was .marginLeft */
+  document.getElementById("main").style.marginRight= "250px"; /* original was .marginLeft */
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
